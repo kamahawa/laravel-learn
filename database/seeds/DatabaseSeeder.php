b116aca6989be912762411be6fce8d406e4ad3af
cuong truong <kamahawa@gmail.com>
2016-08-05 16:31:36 +0700

    seeder : tao du lieu cho table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call("ProductTableSeeder");
+    }
+}
+
+class ProductTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('product')->insert([
+            array('name'=>'Quan Da Banh', 'price' => 50000, 'cate_id' => 2),
+            array('name'=>'Quan Tennis', 'price' => 55000, 'cate_id' => 2),
+            array('name'=>'Quan Vo Thuat', 'price' => 60000, 'cate_id' => 2),
+            array('name'=>'Quan Cau Long', 'price' => 50000, 'cate_id' => 2)
+        ]);
     }
 }
